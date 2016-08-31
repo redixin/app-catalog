bdf21223ac8dfceff1c21d99ca86d3bf39d94391
Sergey Skripnick <sskripnick@mirantis.com>
2016-08-31 14:27:25 +0200

    Add LOCAL_SETTINGS_PATH
    Change-Id: If9b1d75c3f07a5980e06d74e54ad9d2a62f6f88d

--- openstack_catalog/settings.py
+++ openstack_catalog/settings.py
@@ -117,9 +117,11 @@ USE_L10N = True
 USE_TZ = True
 
 # Override some values from local_settings.py if found
-if os.path.isfile('local_settings.py'):
+_LOCAL_SETTINGS_PATH = os.environ.get("LOCAL_SETTINGS_PATH", ".")
+if os.path.isfile(os.path.join(_LOCAL_SETTINGS_PATH, 'local_settings.py')):
     import sys
-    sys.path.insert(0, '.')
+    sys.path.insert(0, _LOCAL_SETTINGS_PATH)
     from local_settings import *  # noqa
+    sys.path.pop(0)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
